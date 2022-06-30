e2d014ba62dabd37e5539923585d5bc478fd0d13
gagan-baghel <78648366+gagan-baghel@users.noreply.github.com>
2022-07-01 01:51:29 +0530

    Add files via upload

---
+++ 02_FactoryMethod/MumbaiVeggiePizza.java
@@ -0,0 +1,18 @@
+class MumbaiVeggiePizza extends Pizza
+{
+  MumbaiVeggiePizza()
+{
+   name = "Mumbai Veggie Pizza";
+   sauce ="tomato ketchup";
+   dough = "Thin dough";
+}
+  public void lotsofveggies()
+  {
+    System.out.println("Adding lots of veggies... ");
+  }
+  public void prepare()
+  {
+    System.out.println("Preparing mumbai veggie pizza...");
+  }
+ 
+}
---
+++ 02_FactoryMethod/NagpurCheesePizza.java
@@ -0,0 +1,18 @@
+class NagpurCheesePizza extends Pizza
+{
+  NagpurCheesePizza()
+  {
+    name = "Nagpur Cheese Pizza";
+    sauce ="tomato ketchup";
+    dough = "Thick dough";
+  }
+  public void lotsofcheese()
+  {
+    System.out.println("Adding lots of cheese... ");
+  }
+  public void prepare()
+  {
+    System.out.println("Preparing nagpur cheese pizza...");
+  }
+ 
+}
